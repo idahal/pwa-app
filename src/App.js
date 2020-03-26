f6524dcde92bb32ba9bc3c6aa383ddb170b9fda2
Ida <ida.hallner@iusinnovation.se>
2020-03-26 14:38:15 +0100

    Just and some comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
 import item from '../src/Images/imageOne.png'
-function App() {
-  // const [data, setData] = useState([])
 
-  // useEffect(() => {
-  //   const fetchData = async() => {
-  //     const result = await fetch('https://jsonplaceholder.typicode.com/photos').then (
-  //       response => response.json()
-  //     )
-  //     setData(result)
-  //   }
-  //   fetchData()
-  // }, [])
+// The app using saved data to work offline
+function App() {
+ 
   return (
     <div className="App">
       <header>
@@ -21,14 +13,41 @@ function App() {
       <div>
       <img src={item} alt="Logo" />
       </div>
-      {/* {data.slice(0,9).map(( item) => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <img src={item.thumbnailUrl} alt="Logo" />
-        </div>  
-      ))} */}
     </div>
   );
 }
 
 export default App;
+
+
+// If the app fetches data form a API 
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [data, setData] = useState([])
+
+//   useEffect(() => {
+//     const fetchData = async() => {
+//       const result = await fetch('https://jsonplaceholder.typicode.com/photos').then (
+//         response => response.json()
+//       )
+//       setData(result)
+//     }
+//     fetchData()
+//   }, [])
+//   return (
+//     <div className="App">
+//       <header>
+//         <h1>Videos</h1>
+//         <h1>Images</h1>
+//       </header>
+//       {data.map(item => (
+//         <div key={item.id}>
+//           <p>{item.title}</p>
+//           <img src={item.thumbnailUrl} alt="Logo" />
+//         </div> 
+//       ))}
+//     </div>
+//   );
+// }
