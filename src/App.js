203aee48bcbd3a230c757fff6c8eab86097f0cd1
Ida <ida.hallner@iusinnovation.se>
2020-03-26 11:01:28 +0100

    Fetch api with fake data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const result = await fetch('https://jsonplaceholder.typicode.com/photos').then (
+        response => response.json()
+      )
+      setData(result)
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
       <header>
-        <h1>Videos</h1>
+        <h1>Images</h1>
       </header>
+      {data.map(item => (
+        <div key={item.id}>
+          <p>{item.title}</p>
+          <img src={item.thumbnailUrl} alt="Logo" />
+        </div> 
+      ))}
     </div>
   );
 }
